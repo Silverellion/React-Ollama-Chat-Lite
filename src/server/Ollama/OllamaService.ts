526d30d2efe6a0e2b18fd613507a15dba1ae4aa1
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-03-12 06:32:36 +0900

    Add custom CSS variables for theming

--- src/server/Ollama/OllamaService.ts
+++ src/server/Ollama/OllamaService.ts
@@ -51,3 +51,4 @@ export default async function OllamaResponse(
 // Updated on 2025-02-18 12:55:28
 // Updated on 2025-02-28 15:28:21
 // Updated on 2025-02-28 12:28:19
+// Updated on 2025-03-12 06:32:36
