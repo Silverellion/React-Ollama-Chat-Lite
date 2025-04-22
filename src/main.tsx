445e25408d891f7e7c84f3120c7c3630e5e9e5ea
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-22 19:32:58 +0900

    Code cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -7,36 +7,3 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>
 );
-// Updated on 2024-05-09 23:33:24
-// Updated on 2024-07-28 11:36:19
-// Updated on 2024-08-29 18:42:17
-// Updated on 2023-05-03 04:24:39
-// Updated on 2023-10-10 18:16:20
-// Updated on 2023-11-06 22:11:57
-// Updated on 2025-02-05 19:10:45
-// Updated on 2025-02-12 07:14:17
-// Updated on 2025-02-16 09:03:27
-// Updated on 2025-02-20 19:44:17
-// Updated on 2025-03-07 19:08:43
-// Updated on 2025-03-10 04:58:03
-// Updated on 2025-03-15 01:19:16
-// Updated on 2025-03-23 00:42:40
-// Updated on 2025-03-24 18:34:39
-// Updated on 2025-03-26 02:53:19
-// Updated on 2025-04-02 19:07:46
-// Updated on 2025-04-03 21:43:57
-// Updated on 2025-02-06 19:16:46
-// Updated on 2025-02-14 11:55:49
-// Updated on 2025-02-22 03:20:06
-// Updated on 2025-02-22 00:01:54
-// Updated on 2025-03-01 20:16:27
-// Updated on 2025-03-04 19:07:05
-// Updated on 2025-03-04 04:08:25
-// Updated on 2025-03-07 00:38:09
-// Updated on 2025-03-07 14:18:10
-// Updated on 2025-03-08 11:34:19
-// Updated on 2025-03-26 04:58:23
-// Updated on 2025-03-31 14:26:17
-// Updated on 2025-04-13 19:42:30
-// Updated on 2025-04-17 10:23:14
-// Updated on 2025-04-17 02:25:04
